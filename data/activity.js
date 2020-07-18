bb4bc22346c44fe4d396bf376c3d9ac4327c015b
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-07-18 22:12:02 +0530

    Create activity.js

---
+++ data/activity.js
@@ -0,0 +1,15 @@
+var activity = [
+{
+  "date": "20-07-2020",
+  "title": "face shield distribution",
+  "content": "we have distributed face shields to health care workers and RTC employees.",
+  "location": "Repalle, Guntur"
+},
+{
+  "date": "26-01-2020",
+  "title": "kids home visit",
+  "content": "we have distributed meals to kids in hostel on the occasion of republic day",
+  "location": "LB Nagar, Hyderabad"
+}
+
+];

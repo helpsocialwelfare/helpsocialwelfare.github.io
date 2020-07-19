153d9f740f6b4243ef0a1a03870652497b8205af
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-07-19 14:33:22 +0530

    Create team.js

---
+++ data/team.js
@@ -0,0 +1,39 @@
+var team = {
+  "committee": {
+    "label": "Commitee",
+    "description":" Commitee members",
+    "members": [
+      {
+        "name": "Guntur Venkata Maruti Prabhakar",
+        "description": "Founder and President",
+        "picture":"../images/team/gvmprabhakar.jpg"
+        "profile_url": "https://www.facebook.com/maruti.prabhakar"
+      },
+      {
+        "name": "Adigopula Bhanu Koti Prakash",
+        "description": "Vice President",
+        "picture":"../images/team/abprakash.jpg"
+        "profile_url": "https://www.facebook.com/prakashabk"
+      },
+      {
+        "name": "Gade Srikanth",
+        "description": "General Secretary",
+        "picture":"../images/team/gsrikanth.jpg"
+        "profile_url": "https://www.facebook.com/gsrikanth"
+      }
+    ]
+  },
+  "others": {
+    "label": "Team",
+    "description":" Other team members",
+    "members": [
+      {
+        "name": "Guntur Venkata Maruti Prabhakar",
+        "description": "Founder and President",
+        "picture":"../images/team/gvmprabhakar.jpg"
+        "profile_url": "https://www.facebook.com/maruti.prabhakar"
+      }
+    ]
+  }
+
+}

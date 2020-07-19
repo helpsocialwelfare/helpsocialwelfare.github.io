ebde0376668fb5cd3d8ab6dee5d423104ff5aaed
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-07-19 16:40:17 +0530

    Update activity.js

--- data/activity.js
+++ data/activity.js
@@ -1,15 +1,17 @@
 var activity = [
 {
-  "date": "20-07-2020",
-  "title": "face shield distribution",
-  "content": "we have distributed face shields to health care workers and petrol bunk employees.",
-  "location": "Repalle, Guntur"
+  date: "20-07-2020",
+  title: "face shield distribution",
+  content: "we have distributed face shields to health care workers and petrol bunk employees.",
+  location: "Repalle, Guntur",
+  totalImages: 3
 },
 {
-  "date": "26-01-2020",
-  "title": "kids home visit",
-  "content": "we have distributed meals to kids in hostel on the occasion of republic day",
-  "location": "LB Nagar, Hyderabad"
+  date: "25-12-2019",
+  title: "kids home visit",
+  content: "we have distributed meals to kids in hostel on the occasion of republic day",
+  location: "LB Nagar, Hyderabad",
+  totalImages: 3
 }
 
 ];

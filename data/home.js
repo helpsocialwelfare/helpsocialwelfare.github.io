3a493565a8399fe31fdb9b5232fbd2fc2263f7dd
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-08-04 16:40:13 +0530

    Create home.js

---
+++ data/home.js
@@ -0,0 +1,78 @@
+var home = {
+  slides: [
+    {
+        title: `HELP Social Welfare Association`
+      , description: `Dr. Abdul Kalam said "YOU were born with WINGS, Don't crawl, learn to use them fly and fly"! 
+                      The question is.. "How many realize they CAN stretch wings?" At some point of time in life, many of us alter the destination due to a missing help!
+                      Be it Health, Education, Lifestyle or Path finder (Career Guidance), WE, the HELPers, believe in fulfilling this gap & enable people to resume their journeys!`
+      , image: ``
+    },
+    {
+      title: `face shield distribution`,
+      description: `we have distributed face shields to health care workers and petrol bunk employees.`,
+      image: ``
+    },
+    {
+      title: `kids home visit`,
+      description: `we have distributed meals to kids in hostel on the occasion of republic day`,
+      image: ``
+    }
+  ],
+  cards: [
+    {
+        title: `Health`
+      , description: `Health is the primary thing after which everything else follows. 
+                      Be it sudden or chronic, we have seen where a family lands when a family member requires medical help. 
+                      We, the HELPers, contribute the deceased in such tough times by verifying authenticity.`
+      , image: ``
+    },
+    {
+      title: `Education`
+      , description: `Life is a real-time game but surely, Education is the torch to concur the dark. 
+                      We, the HELPers, are on a mission to assist the needy students monetarily to continue their studies.`
+      , image: ``
+    },
+    {
+      title: `Lifestyle`
+      , description: `The broader sense of lifestyle is a "way or style of living" but even today, we see many people in our society that face challenges for basic needs. 
+                      We, the HELPers, try reaching out to them and assist in fulfilling at least some of their needs.`
+      , image: ``
+    },
+    {
+      title: `Pathfinder`
+      , description: `As a student, we all have aspirations on what we wanted to become but were not clear on the 'how' part. 
+                      Many have the virtue of educated parents and/or relatives but we wish to be the source & direction for the rest. 
+                      We, the HELPers, visit rural schools, conduct career orientation programs at the schools for Classes IX & X, explain our personal experiences and distribute hand books that have all possible paths after class-X, XII etc. for ITI, to Teacher, Doctor, Engineer and many more.`
+      , image: ``
+    }
+  ],
+  about: {
+    description: `Founded on 26th January 2016 by a group of friends headed by Shri. G.V.M.Prabhakar, our HELP touched many lives in each of these categories. 
+                  Our continuous HELP to society brought nothing but just smiles on numerous faces! Our gallery reflects this very well. Please do visit it.
+                  Formally.. 
+                  "YOU are WELCOME to HELP!"`
+    , team:  [
+      {
+        name: `Guntur Venkata Maruti Prabhakar`,
+        title: `Founder and President`,
+        description: ``,
+        image: `./data/images/team/gvmprabhakar.jpg`,
+        profile_url: `https://www.facebook.com/maruti.prabhakar`,
+      },
+      {
+        name: `Adigopula Bhanu Koti Prakash`,
+        title: `Vice President`,
+        description: ``,
+        image: `./data/images/team/abprakash.jpg`,
+        profile_url: `https://www.facebook.com/prakashabk`,
+      },
+      {
+        name: `Gade Srikanth`,
+        title: `General Secretary`,
+        description: ``,
+        image: `./data/images/team/gsrikanth.jpg`,
+        profile_url: `https://www.facebook.com/gsrikanth`,
+      }
+    ]
+  }
+}

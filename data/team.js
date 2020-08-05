e456735a5a491daa87c9c7da442b39329a215676
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-08-05 20:41:43 +0530

    Update team.js

--- data/team.js
+++ data/team.js
@@ -1,42 +1,182 @@
 var team = {
-  "committee": {
     "label": "Commitee",
     "description":" Commitee members",
     "members": [
       {
-        "name": "Guntur Venkata Maruti Prabhakar",
-        "title": "Founder and President",
+        "name": "G.V.M Prabhakar",
+        "city": "Hyderabad",
+        "company": "Intense Technologies Limited",
         "description": "",
+        "designation":"Software Engineer",
+        "education": "MCA",
         "picture":"./data/images/team/gvmprabhakar.jpg",
-        "profile_url": "https://www.facebook.com/maruti.prabhakar"
+        "profile_url": "https://www.facebook.com/maruti.prabhakar",
+        "title": "President"
       },
       {
-        "name": "Adigopula Bhanu Koti Prakash",
-        "title": "Vice President",
+        "name": "A.B.K Prakash",
+        "city": "Hyderabad",
+        "company": "JP Morgan India Pvt. Ltd",
         "description": "",
+        "designation":"Software Engineer",
+        "education": "MCA",
         "picture":"./data/images/team/abprakash.jpg",
-        "profile_url": "https://www.facebook.com/prakashabk"
+        "profile_url": "https://www.facebook.com/prakashabk",
+        "title": "Vice President"
       },
       {
         "name": "Gade Srikanth",
-        "title": "General Secretary",
+        "city": "Hyderabad",
+        "company": "Pega Systems",
         "description": "",
+        "designation":"Associate Architect",
+        "education": "MCA",
         "picture":"./data/images/team/gsrikanth.jpg",
-        "profile_url": "https://www.facebook.com/gsrikanth"
-      }
-    ]
-  },
-  "others": {
-    "label": "Team",
-    "description":" Other team members",
-    "members": [
+        "profile_url": "https://www.facebook.com/gsrikanth",
+        "title": "General Secretary"
+      },
       {
-        "name": "Guntur Venkata Maruti Prabhakar",
-        "description": "Founder and President",
-        "picture":"./data/images/team/gvmprabhakar.jpg",
-        "profile_url": "https://www.facebook.com/maruti.prabhakar"
-      }
+        "name": "CHANDRA SEKAR GOURISETTY",
+        "city": "London",
+        "company": "ATOS SYNTEL",
+        "description": "",
+        "designation":"Project Lead",
+        "education": "MCA",
+        "picture":"./data/images/team/gchandrasekhar.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "K.V.S Sastry",
+        "city": "Hyderabad",
+        "company": "DRDO",
+        "description": "",
+        "designation":"Scientist-E",
+        "education": "M.Tech,IIT Kharagpur",
+        "picture":"./data/images/team/kvssastry.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Ramakrishna Pasupuleti",
+        "city": "Hyderabad",
+        "company": "Intense Technologies Limited",
+        "description": "",
+        "designation":"Software Engineer",
+        "education": "MCA",
+        "picture":"./data/images/team/pramakrishna.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Rajesh Devisetti",
+        "city": "Hyderabad",
+        "company": "Intense Technologies Limited",
+        "description": "",
+        "designation":"Software Engineer",
+        "education": "MCA",
+        "picture":"./data/images/team/drajesh.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "GOPINADH KANDULA",
+        "city": "Hyderabad",
+        "company": "Intense Technologies Limited",
+        "description": "",
+        "designation":"Software Engineer",
+        "education": "MCA",
+        "picture":"./data/images/team/kgopinadh.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Siva Subrahmanyam K",
+        "city": "Hyderabad",
+        "company": "JP Morgan India Pvt. Ltd",
+        "description": "",
+        "designation":"Software Engineer",
+        "education": "MCA",
+        "picture":"./data/images/team/ksivasubrahmanyam.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "M Venkateswara Rao",
+        "city": "Hyderabad",
+        "company": "Accenture",
+        "description": "",
+        "designation":"Team Lead",
+        "education": "MCA",
+        "picture":"./data/images/team/mvenkateswararao.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Sridhar babu G",
+        "city": "Bengaluru",
+        "company": "Accenture",
+        "description": "",
+        "designation":"Team Lead",
+        "education": "MCA",
+        "picture":"./data/images/team/gsridharbabu.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "K.N. Bapaiah",
+        "city": "Hyderabad",
+        "company": "Kalyani communications Pvt Ltd.",
+        "description": "",
+        "designation":"Asst. Branch Manager",
+        "education": "MCA",
+        "picture":"./data/images/team/knbapaiah.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Madhava Rao Gunturu",
+        "city": "Hyderabad",
+        "company": "IBM India Pvt. Ltd",
+        "description": "",
+        "designation":"Team Lead",
+        "education": "MCA",
+        "picture":"./data/images/team/gmadhavarao.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Srinivasarao Kalisetty",
+        "city": "Ahmedabad",
+        "company": "Karur Vysya Bank LTD",
+        "description": "",
+        "designation":"Asst. Manager",
+        "education": "MCA",
+        "picture":"./data/images/team/ksrinivasarao.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "P Krishna Kishore",
+        "city": "Hyderabad",
+        "company": "Wipro",
+        "description": "",
+        "designation":"Senior Software Engineer",
+        "education": "MSc(Computers)",
+        "picture":"./data/images/team/pkrishnakishore.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
+      {
+        "name": "Gopinath Puppala",
+        "city": "Hyderabad",
+        "company": "Phenom People",
+        "description": "",
+        "designation":"Tech Lead",
+        "education": "MTech",
+        "picture":"./data/images/team/pgopinath.jpg",
+        "profile_url": "https://www.facebook.com/",
+        "title": ""
+      },
     ]
-  }
-
 }

e3e9d795320065631ab4a94cfe06fb4184a04c35
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-08-07 21:53:47 +0530

    Update activity.js

--- data/activity.js
+++ data/activity.js
@@ -2,21 +2,21 @@ var activities = [
   {
     date: "20-07-2020",
     title: "Face shield distribution",
-    description: "Distributed face shields to health care workers of Govt. hospitals",
+    description: "Distributed face shields to health care workers of Govt. hospitals.",
     location: "Repalle, Avanigadda",
     totalImages: 6
   },
   {
     date: "26-01-2020",
     title: "Food donation at 'GLOBAL HOPE INDIA' ",
-    description: "Served meals to kids at GLOBAL HOPE INDIA orphanage home on the occasion of republic day",
+    description: "Served meals to kids at GLOBAL HOPE INDIA orphanage home on the occasion of republic day.",
     location: "LB Nagar, Hyderabad",
     totalImages: 6
   },
   {
     date: "25-12-2019",
     title: "Food donation at Helen Keller's Institute",
-    description: "Served meals to kids at Keller's Institute of research & rehabilitation for disabled children on the occasion of republic day",
+    description: "Served meals to kids at Keller's Institute of research & rehabilitation for disabled children on the occasion of christmas.",
     location: "Neredmet, Hyderabad",
     totalImages: 6
   }

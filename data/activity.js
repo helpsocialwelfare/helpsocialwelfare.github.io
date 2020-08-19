57f8eeae7d95d3e1eb37e80c62dd0d0863c813b5
helpsocialwelfare <68437621+helpsocialwelfare@users.noreply.github.com>
2020-08-19 20:01:13 +0530

    Update activity.js

--- data/activity.js
+++ data/activity.js
@@ -18,6 +18,48 @@ var activities = [
     title: "Food donation at Helen Keller's Institute",
     description: "Served meals to kids at Keller's Institute of research & rehabilitation for disabled children on the occasion of christmas.",
     location: "Neredmet, Hyderabad",
+    totalImages: 10
+  },
+  {
+    date: "05-01-2019",
+    title: "Motivation & Career Guidance Session to 9th & 10th class students",
+    description: "Conducted Career Guidance Session to 9th & 10th class students to explore the future opportunities",
+    location: "Gullapalli, Rajolu, Ilavaram, Bhattiprolu - Repalle",
+    totalImages: 13
+  },
+  {
+    date: "27-10-2018",
+    title: "Motivation & Career Guidance Session to 9th & 10th class students",
+    description: "Conducted Career Guidance Session to 9th & 10th class students to explore the future opportunities",
+    location: "SSS Muncipal High school, Municipall Upper primary school - Repalle",
     totalImages: 6
+  },
+  {
+    date: "30-12-2017",
+    title: "Motivation & Career Guidance Session to 9th & 10th class students",
+    description: "Conducted Career Guidance Session to 9th & 10th class students to explore the future opportunities",
+    location: "Pesarlanka, Peteru - Repalle",
+    totalImages: 11
+  },
+  {
+    date: "25-11-2017",
+    title: "Motivation & Career Guidance Session to 9th & 10th class students",
+    description: "Conducted Career Guidance Session to 9th & 10th class students to explore the future opportunities",
+    location: "Velmajala, Jangaon, Sharajipeta, Yadhagirigutta - Telangana",
+    totalImages: 7
+  }, 
+  {
+    date: "18-06-2017",
+    title: "Food & Fruit distribution at old age home",
+    description: "Offered Food & Fruits to senior citizens at old age home",
+    location: "Nalluru, Repalle, Guntur",
+    totalImages: 3
+  },
+  {
+    date: "29-04-2017",
+    title: "Fruit distribution at Govt Hospital",
+    description: "Distributed fruits to patients of Government Hospital",
+    location: "Repalle, Guntur",
+    totalImages: 5
   }
 ];
